08bdbdd5a1b42ec1536f1dcb8c31c9360ae5b5b5
Evan Drake <evanmdrake@icloud.com>
2022-07-26 14:19:14 -0700

    added second ssh key

--- aws/ec2-script.sh
+++ aws/ec2-script.sh
@@ -1,16 +1,24 @@
 # now included in script git
 case $1 in
   'run-instance')
-    aws-vault exec dev-sandbox -- aws ec2 run-instances --image-id ami-02eac2c0129f6376b --count 1 --instance-type t2.micro --key-name  evandrake-dob-pair \
+    aws-vault exec dev-sandbox -- aws ec2 run-instances --image-id ami-02eac2c0129f6376b --count 1 --instance-type t2.micro --key-name  $2 \
       --security-groups evandrake-bootcamp
     ;;
+    # $2 keypair
 
   'name')
-    aws-vault exec $2 -- aws ec2 create-tags --resources $3 --tags "Key=Name,Value=$4-jenkins-master"
+    aws-vault exec $2 -- aws ec2 create-tags --resources $3 --tags "Key=Name,Value=$4"
+    ;;
+    # $2 enviornment
+    # $3 ID
+    # $4 my name
+
+  'ssh1')
+    ssh -i "evandrake-dob-pair.pem" centos@ec2-54-210-215-29.compute-1.amazonaws.com
     ;;
 
-  'ssh')
-    ssh -i "evandrake-dob-pair.pem" centos@ec2-54-210-215-29.compute-1.amazonaws.com
+  'ssh2')
+    ssh -i "evandrake-jenkins-agent.pem" centos@ec2-3-87-194-206.compute-1.amazonaws.com
     ;;
 
   'stop')
