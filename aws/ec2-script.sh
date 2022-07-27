146c5b9b71c761ba39e00e28b0d9f76db39a49f7
Evan Drake <evanmdrake@icloud.com>
2022-07-26 17:59:56 -0700

    added help script to ec2

--- aws/ec2-script.sh
+++ aws/ec2-script.sh
@@ -3,11 +3,13 @@ case $1 in
   'run-instance')
     aws-vault exec dev-sandbox -- aws ec2 run-instances --image-id ami-02eac2c0129f6376b --count 1 --instance-type t2.micro --key-name  $2 \
       --security-groups evandrake-bootcamp
+    exit
     ;;
     # $2 keypair
 
   'name')
     aws-vault exec $2 -- aws ec2 create-tags --resources $3 --tags "Key=Name,Value=$4"
+    exit
     ;;
     # $2 enviornment
     # $3 ID
@@ -15,20 +17,24 @@ case $1 in
 
   'ssh1')
     ssh -i evandrake-dob-pair.pem centos@ec2-54-210-215-29.compute-1.amazonaws.com
+    exit
     ;;
 
   'ssh2')
     ssh -i "evandrake-jenkins-agent.pem" centos@ec2-3-87-194-206.compute-1.amazonaws.com
+    exit
     ;;
 
   'stop')
     aws-vault exec $2 -- aws ec2 stop-instances --instance-ids $3
+    exit
     ;;
     # $2 ex: dev-sandbox
     # $3 ID
 
   'start')
     aws-vault exec $2 -- aws ec2 start-instances --instance-ids $3
+    exit
     ;;
     # $2 ex: dev-sandbox
     # $3 ID
@@ -42,8 +48,23 @@ case $1 in
     aws-vault exec "$2" -- aws ec2 terminate-instances --instance-ids "$3"
     echo
     echo ITS GONE NOW
+    exit
     ;;
     # $2 dev environment
     # $3 ID
 
 esac
+
+if [[ $1 = '^h(elp)' | $1 = * ]]; then
+  echo
+  echo run-instance [ID]
+  echo
+  echo name [enviornment] [ID] [your name]
+  echo
+  echo ssh[1|2] <- configure to your instance
+  echo
+  echo stop <- [enviornment] [ID]
+  echo
+  echo perm-delete [enviornment] [ID]
+  echo
+fi
